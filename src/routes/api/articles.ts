a01ca6fcad26ac8ee4dc5a2b35c9387e10144040
Florian Chevallier <florian.chevallier@in-tact.fr>
2022-03-07 11:20:22 +0100

    feat: remove pupeteer for tests

--- src/routes/api/articles.ts
+++ src/routes/api/articles.ts
@@ -1,22 +1,24 @@
 // Require the package
-import googleNewsScraper from 'google-news-scraper';
+// import googleNewsScraper from 'google-news-scraper';
 import type { IArticle } from 'src/types/types'
 
 
 export async function get() {
 
 	// Execute within an async function, pass a config object (further documentation below)
-	const articles: IArticle[] = await googleNewsScraper({
-		searchTerm: "Pokémon",
-		prettyURLs: false,
-		queryVars: {
-			hl: "fr-FR",
-			gl: "Fr",
-			ceid: "US:en"
-		},
-		timeframe: "7d",
-		puppeteerArgs: []
-	});
+	// const articles: IArticle[] = await googleNewsScraper({
+	// 	searchTerm: "Pokémon",
+	// 	prettyURLs: false,
+	// 	queryVars: {
+	// 		hl: "fr-FR",
+	// 		gl: "Fr",
+	// 		ceid: "US:en"
+	// 	},
+	// 	timeframe: "7d",
+	// 	puppeteerArgs: []
+	// });
+
+	const articles = [];
 
 	if (articles) {
 		const finalArticles = articles.map(a => ({ ...a, time: a.time.split('ago')[0] }));
